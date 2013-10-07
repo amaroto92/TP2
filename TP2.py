baa1115a94042d600ca1192c9006a71364eae990
Andrés Maroto <amaroto92@gmail.com>
2013-10-06 19:54:42 -0600

    Mayusculas arreglado

--- TP2.py
+++ TP2.py
@@ -5,7 +5,7 @@ BaseConocimientos=['nl','fail','write(args)'] #Lista que contiene la BC y le agr
 
 
 global Lexico
-Lexico = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',',','.','(',')','[',']','-',':']
+Lexico = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',',','.','(',')','[',']','-',':','_']
 
 
 #### Menu de Inicio ####################################################################
@@ -52,7 +52,7 @@ def MenuAdministrativo():
 #### Ingresar Hechos ####################################################################
 def IngresarHecho():
 	print
-	print(" Ingresar Hechos")
+	print("Ingresar Hechos")
 	print("*Un hecho debe terminar con un punto(.) al final")
 	print("*Un hecho puede tener 0 a N argumentos")
 	print("Ejemplo: perro(bruno). amigo(daniel,felipe).")
@@ -80,21 +80,22 @@ def IngresarHecho_Aux():
 		else:
 			print
 			return MenuAdministrativo()
-		
+
+# Funcion para validar el lexico del hecho:		
 
 def ScannerHecho(hecho):
 	for caracter in hecho:
-		if caracter in Lexico[:-2]:
+		if caracter.lower() in Lexico[:-2]:
 			True
 		else:
 			print("Error de scanner: "+caracter+" no es token 				valido")
 			return False
 	return True
 	
-    
+# Funcion para validar la sintaxis del hecho :   
 
 def ParserHecho(hecho):
-	print(hecho)
+	
 	banderaparentisis = False
 	if hecho[-1] != '.': #Que termine con punto.
 		print("Un hecho tiene que terminar con punto")
@@ -106,7 +107,7 @@ def ParserHecho(hecho):
 			print (hecho[i])
 
 			if i==0 and (hecho[i].islower() == False): # Que 				empieze con minuscula.
-				print("Un hecho tiene que empezar con 					Mayuscula")
+				print("Un hecho tiene que empezar con Minuscula")
 				return False
 		
 			elif hecho[i] == '(' :
@@ -134,7 +135,55 @@ def ParserHecho(hecho):
 
 #### Ingresar Regla ###################################################################
 def IngresarRegla():
-	print("Ingresar Regla")
+	print
+	print(" Ingresar Reglas")
+	print("*Una Regla debe terminar con un punto(.) al final")
+	print("*Una Regla puede tener 0 a N argumentos")
+	print("*Toda Regla puede tener N antecedentes")
+	print("Ejemplo: amigo(daniel,felipe).")
+	print
+	return IngresarRegla_Aux()
+
+def IngresarRegla_Aux():
+	regla=raw_input(": ")
+	if ScannerRegla(regla) and ParserRegla(regla): #Si pasa las etapas de Scanner y Parser correctamente, agrega el hecho a la BC.
+
+		BaseConocimientos.append(regla) #append lo agrega al final de 							la lista.
+		print("Regla ingresado")
+		opcion=input("Regla ingresada, presione 1 para agregar nueva regla y 2 para volver al menu: ")
+		if opcion==1:
+			return IngresarRegla_Aux()
+		else:
+			print
+			return MenuAdministrativo()
+	else:
+		print("Error al ingresar regla a la Base de Conocimiento") 
+		#No se agrega nada.
+		opcion=input("presione 1 para agregar nueva regla y 2 para volver al menu: ")
+		if opcion==1:
+			return IngresarRegla_Aux()
+		else:
+			print
+			return MenuAdministrativo()
+		
+
+# Funcion para validar el lexico de la regla
+def ScannerRegla(regla):
+	
+	for caracter in regla:
+		if caracter.lower() in Lexico:
+			True
+		else:
+			print("Error de scanner: "+caracter+" no es token valido")
+			return False
+	return True
+
+# Funcion para validar la sintaxis de la regla
+
+def ParserRegla(regla):
+
+# FALTA!!
+
 
 
 #### Mostrar Hechos ###################################################################
